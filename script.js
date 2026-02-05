38998640c1d89624f991f055786097bc93fe63bd
DavidPopovich3261 <8534397@gmail.com>
2026-02-05 17:22:14 +0200

    finish

--- script.js
+++ script.js
@@ -10,12 +10,13 @@ const score1 = document.getElementById("score1")
 const score2 = document.getElementById("score2")
 const plyer1 = document.getElementById("plyer1")
 const plyer2 = document.getElementById("plyer2")
-const maxscore=document.getElementById("maxscore")
+const maxscore = document.getElementById("maxscore")
 let max = 20
-maxscore.innerText= JSON.stringify(max)
+maxscore.innerText = JSON.stringify(max)
 let rdice1
 let rdice2
 let plyer = 1
+plyer1.className = "pley"
 console.log(maxscore);
 
 hold.addEventListener("click", () => {
@@ -25,26 +26,32 @@ hold.addEventListener("click", () => {
         score1.innerText = Number(score1.innerText) + Number(cor1.innerText)
         cor1.innerText = 0
         if (score1.innerText >= max) {
-            plyer1.className = "win"   
-            const winner= document.createElement("h1")     
-            winner.innerText="plyrt 1 is a winner"  
-            plyer1.insertBefore(winner,score1) 
+            plyer1.className = "win"
+            const winner = document.createElement("h1")
+            winner.innerText = "plyrt 1 is a winner"
+            plyer1.insertBefore(winner, score1)
             roll.style.pointerEvents = "none"
             hold.style.pointerEvents = "none"
         }
         plyer = 2
+        plyer1.classList.remove ( "pley")
+        plyer2.className = "pley"
+
     } else {
         score2.innerText = Number(score2.innerText) + Number(cor2.innerText)
         cor2.innerText = 0
         if (score2.innerText >= max) {
-            plyer2.className ="win"
-            const winner= document.createElement("h1")     
-            winner.innerText="plyrt 2 is a winner"  
-            plyer2.insertBefore(winner,score2) 
+            plyer2.className = "win"
+            const winner = document.createElement("h1")
+            winner.innerText = "plyrt 2 is a winner"
+            plyer2.insertBefore(winner, score2)
             roll.style.pointerEvents = "none"
             hold.style.pointerEvents = "none"
         }
         plyer = 1
+        plyer2.classList.remove ("pley")
+        plyer1.className = "pley"
+
     }
 
 
